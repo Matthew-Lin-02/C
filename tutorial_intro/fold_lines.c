67816a06925916d2f9ecf2038699ca947ca7a3a5
Matthew Lin <matthew.lin@redtrain.com.au>
2026-03-16 14:44:53 +1100

    some garbage

--- tutorial_intro/fold_lines.c
+++ tutorial_intro/fold_lines.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #define MAXLINE 1000
 #define FOLDCOLUMN 10
+#define TABSTOP 8
 /* maximum input line lengths */
 int kgetline(char line[], int maxline);
 int calculate_spaces_to_tabstop(int offset, int tab_size);
@@ -11,16 +12,24 @@ int main(void)
     char line[MAXLINE];
 
     while ((len = kgetline(line, MAXLINE)) > 0) {
-        int start = 0;
-        while(start < len){
-
-            int end = (len - start < FOLDCOLUMN) ? len - i : FOLDCOLUMN; //non inclusive
-            
-            if(line[i] != ' ' && line[i] != '\t'){
-
-            }else{
-                printf(len[])
+        int i = 0;
+        int j = 0;
+        int column = 0;
+        while(i < len){
+            // must iterate through regardless due to existence of tabs which would but the column in an odd position
+            int last_space_index = i;
+            while(i < len && column < FOLDCOLUMN){
+                if(line[i] == '\t'){
+                    column += calculate_spaces_to_tabstop(column, TABSTOP);   
+                }else{
+                    column +=1;
+                }
+                ++i;
             }
+
+            int last_space = 0;
+                i+=1
+            int end = (len - start < FOLDCOLUMN) ? len - i : FOLDCOLUMN; //non inclusive
 
             start += FOLDCOLUMN;
         }
